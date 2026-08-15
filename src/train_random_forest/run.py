2e06e6df26a95e79f74089e3c362d55b44e22b86
Valshiran <leftythedrummer88@gmail.com>
2026-08-14 22:27:51 -0600

    Added the sk_pipe fit code to run.py in train_random_forest

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -73,7 +73,7 @@ def go(args):
 
     ######################################
     # Fit the pipeline sk_pipe by calling the .fit method on X_train and y_train
-    # YOUR CODE HERE
+    sk_pipe.fit(X_train, y_train)
     ######################################
 
     # Compute r2 and MAE
@@ -231,7 +231,7 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
         ("random_forest", random_forest)
         ]
     )
-
+    
     return sk_pipe, processed_features
     ######################################
 
